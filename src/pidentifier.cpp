450e4dac3cf7c2640e3170209be0c1fb29048e7f
Peefy <xpf6677@163.com>
2019-12-09 18:36:59 +0800

    Finish chapter 2

--- src/pidentifier.cpp
+++ src/pidentifier.cpp
@@ -9,8 +9,8 @@ static const char* const keywords[] = {
       "open", "read", "close", "printf", "malloc", "free", "memset", "memcmp", "exit", "void", "main", 
       "var", "auto", "where", "in", "out", "typedef", "typename", "nameof", "string",
       "float", "double", "uint", "uint8", "uint16", "uint32", "uint64", "int8", "int16", "int32", "int64", "float32", "float64"
-      "try", "catch", "throw", "raise", "except", "error", "operator", 
-      "override", "virtual", "async", "await", "export", "import", "do", "repeat", "until", "do", "begin", "end", "goto",
+      "try", "catch", "throw", "raise", "except", "error", "operator", "nil", "none", "null", "nullptr", "None", 
+      "override", "virtual", "async", "await", "export", "import", "do", "repeat", "until", "do", "begin", "end", "goto", "pass",
       "::", "!=", "==", "<=", ">=", "<<", ">>", "||", "&&"
 };
 
